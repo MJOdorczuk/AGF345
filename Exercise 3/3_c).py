9e261ee92a2966050d50369502516025a48af3d2
Ines2D <142997340+Ines2D@users.noreply.github.com>
2024-11-26 15:33:21 +0100

    Update 3_c).py

--- Exercise 3/3_c).py
+++ Exercise 3/3_c).py
@@ -7,16 +7,12 @@ import matplotlib.pyplot as plt
 m_e = 9.11e-31  # Electron mass (kg)
 e = 1.6e-19     # Elementary charge (C)
 j_I = CHOOSE IT  # Current density (A/m^2)
-B_0 = CHOOSE IT      # Magnetic field strength at Earth's surface (T)
-n_0 = CHOOSE IT       # Plasma density at Earth's surface (m^3)
+B_h = FROM THE RAW DATA
+n_h = FROM THE RAW DATA
 R_0 = 6.37e6    # Earth's radius (m)
 
 # Altitude range from 100 km to 1000 km
 altitudes = np.linspace(100e3, 1000e3, 100)  # in meters
-
-# Magnetic field and plasma density as functions of altitude
-B_h = B_0 * (R_0 / altitudes)**2  # Magnetic field strength as a function of altitude
-n_h = n_0 * (R_0 / altitudes)**3  # Plasma density as a function of altitude
 
 # Calculate acceleration potential for each altitude
 Phi_h = (m_e * j_I**2) / (2 * e**3 * B_h**2) * (B_h / n_h)**2
